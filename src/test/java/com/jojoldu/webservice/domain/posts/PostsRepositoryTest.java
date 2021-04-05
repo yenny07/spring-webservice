f786b839a65c11723d7d3936192fbef14f6028a1
Yein <flocawint@naver.com>
2021-04-05 17:31:21 +0900

    test: 게시글 불러오기 테스트 작성

---
+++ src/test/java/com/jojoldu/webservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.jojoldu.webservice.domain.posts;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("Yeiiiiin")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo("테스트 게시글");
+        assertThat(posts.getContent()).isEqualTo("테스트 본문");
+   }
+}
