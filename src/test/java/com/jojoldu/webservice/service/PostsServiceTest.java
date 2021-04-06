f6d73e97c745fb946b2d062ad4d166e6b52c0eb0
Yein <flocawint@naver.com>
2021-04-06 15:18:24 +0900

    test : 게시글 등록 test

---
+++ src/test/java/com/jojoldu/webservice/service/PostsServiceTest.java
@@ -0,0 +1,49 @@
+package com.jojoldu.webservice.service;
+
+import com.jojoldu.webservice.domain.posts.Posts;
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PostsServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Dto데이터가_posts테이블에_저장된다() {
+        //given
+        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
+                .author("dto builder author")
+                .content("dto builder content")
+                .title("dto builder title")
+                .build();
+
+        //when
+        postsService.save(dto);
+
+        //then
+        Posts posts = postsRepository.findAll().get(0);
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+    }
+}
