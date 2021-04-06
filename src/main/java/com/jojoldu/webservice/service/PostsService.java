1e5e110e2db9b8b5b04f743702cf940a1449e24c
Yein <flocawint@naver.com>
2021-04-06 15:18:10 +0900

    feat : 게시글 등록 Service

---
+++ src/main/java/com/jojoldu/webservice/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.jojoldu.webservice.service;
+
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto) {
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+}
