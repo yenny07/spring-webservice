a265b2cb331d4d2646364ff2998a38a1d0745907
Yein <flocawint@naver.com>
2021-04-08 18:52:26 +0900

    feat : 게시글 목록 출력 기능

--- src/main/java/com/jojoldu/webservice/service/PostsService.java
+++ src/main/java/com/jojoldu/webservice/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.jojoldu.webservice.service;
 
 import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.posts.PostsMainResponseDto;
 import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -14,5 +18,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/jojoldu/webservice/web/webController.java
+++ src/main/java/com/jojoldu/webservice/web/webController.java
@@ -1,15 +1,20 @@
 package com.jojoldu.webservice.web;
 
+import com.jojoldu.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class webController {
 
+    private PostsService postsService;
+
     @GetMapping("/") // @RequestMapping(value="/", method=RequestMethod.GET)
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
